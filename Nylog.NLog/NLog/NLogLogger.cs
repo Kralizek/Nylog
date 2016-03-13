0b75821aa73c7490241d0776cc927787abb126b7
Renato Golia <golia.renato@gmail.com>
2016-03-13 19:11:37 +0100

    Added NLog implementation

---
+++ Nylog.NLog/NLog/NLogLogger.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using INLogLogger = global::NLog.ILogger;
+using NLogLogLevel = global::NLog.LogLevel;
+
+namespace Nylog.NLog
+{
+    public class NLogLogger : ILogger
+    {
+        private readonly INLogLogger _logger;
+
+        public NLogLogger(INLogLogger logger)
+        {
+            if (logger == null) throw new ArgumentNullException(nameof(logger));
+            _logger = logger;
+
+        }
+        
+        public void Log(LogLevel level, IDictionary<string, object> state, Exception exception)
+        {
+            var nlogLogLevel = LogLevels[level];
+
+            string message = null;
+
+            if (state.ContainsKey("message"))
+            {
+                message = (string)state["message"];
+            }
+
+            var eventInfo = CreateLogEventInfo(nlogLogLevel, message, state, exception);
+
+            _logger.Log(eventInfo);
+
+        }
+
+        private LogEventInfo CreateLogEventInfo(NLogLogLevel level, string message, IDictionary<string, object> dictionary, Exception exception)
+        {
+            LogEventInfo logEvent = new LogEventInfo(level, _logger.Name, message);
+
+            foreach (var item in dictionary)
+            {
+                logEvent.Properties[item.Key] = item.Value;
+            }
+
+            if (exception != null)
+            {
+                logEvent.Properties["error-source"] = exception.Source;
+                logEvent.Properties["error-class"] = exception.TargetSite.DeclaringType.FullName;
+                logEvent.Properties["error-method"] = exception.TargetSite.Name;
+                logEvent.Properties["error-message"] = exception.Message;
+
+                if (exception.InnerException != null)
+                {
+                    logEvent.Properties["inner-error-message"] = exception.InnerException.Message;
+                }
+            }
+
+            return logEvent;
+        }
+
+        public bool IsEnabled(LogLevel level)
+        {
+            return _logger.IsEnabled(LogLevels[level]);
+        }
+
+        private static readonly IReadOnlyDictionary<LogLevel, NLogLogLevel> LogLevels = new Dictionary<LogLevel, NLogLogLevel>
+        {
+            [LogLevel.Verbose] = NLogLogLevel.Trace,
+            [LogLevel.Debug] = NLogLogLevel.Debug,
+            [LogLevel.Information] = NLogLogLevel.Info,
+            [LogLevel.Warning] = NLogLogLevel.Warn,
+            [LogLevel.Error] = NLogLogLevel.Error,
+            [LogLevel.Critical] = NLogLogLevel.Fatal
+        };
+    }
+}
